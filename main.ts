499a14421da9ef1def3f3d94592999095d0c09a7
Jose OC <jose.oc@gmail.com>
2024-06-20 22:19:51 +0200

    Random pause

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     Laser = game.createSprite(Ship.get(LedSpriteProperty.X), Ship.get(LedSpriteProperty.Y))
     for (let index = 0; index < 5; index++) {
-        basic.pause(100)
+        basic.pause(randint(50, 100))
         Laser.change(LedSpriteProperty.Y, -1)
     }
     if (Laser.isTouching(Alien)) {
@@ -28,7 +28,7 @@ game.startCountdown(60000)
 basic.forever(function () {
     Alien.move(1)
     Alien.ifOnEdgeBounce()
-    basic.pause(500)
+    basic.pause(randint(200, 500))
     if (Alien.isDeleted()) {
         Alien = game.createSprite(0, 0)
         Alien.set(LedSpriteProperty.Blink, 8)
@@ -36,7 +36,7 @@ basic.forever(function () {
     if (Alien.get(LedSpriteProperty.X) == Ship.get(LedSpriteProperty.X)) {
         alienBomb = game.createSprite(Alien.get(LedSpriteProperty.X), Alien.get(LedSpriteProperty.Y))
         for (let index = 0; index < 5; index++) {
-            basic.pause(500)
+            basic.pause(randint(200, 500))
             alienBomb.change(LedSpriteProperty.Y, 1)
             if (alienBomb.isTouching(Ship)) {
                 game.gameOver()
